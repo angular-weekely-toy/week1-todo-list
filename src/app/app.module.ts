46b9aa502d89319ab002d522c12e248c18a684d5
sang-a-lee <sang-a.lee@navercorp.com>
2021-11-06 03:48:26 +0900

    add modules, language

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import localeKo from '@angular/common/locales/ko';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoComponent } from './todo/todo.component';
+import { registerLocaleData } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent, TodoListComponent, TodoComponent],
-  imports: [BrowserModule, NgbModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  providers: [{ provide: LOCALE_ID, useValue: 'ko' }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    registerLocaleData(localeKo);
+  }
+}
