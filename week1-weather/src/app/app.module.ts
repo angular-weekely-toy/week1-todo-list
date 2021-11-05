602ec2125d07cb4fed229c494e9a828876d2b40b
rudy <rudy@jaranda.kr>
2021-11-06 00:55:06 +0900

    default structure

--- week1-weather/src/app/app.module.ts
+++ week1-weather/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherFormComponent } from './weather-form/weather-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ week1-weather/src/app/weather-form/weather-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-form',
+  templateUrl: './weather-form.component.html',
+  styleUrls: ['./weather-form.component.css']
+})
+export class WeatherFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
