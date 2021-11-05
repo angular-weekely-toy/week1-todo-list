5af0b5018372c50624c7f1534f210afa2bfa9b9f
sang-a-lee <sang-a.lee@navercorp.com>
2021-11-06 03:48:12 +0900

    add todo list, todo

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
 import { Todo } from '../shared/todo';
 import { TodoService } from '../todo.service';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-todo-list',
@@ -8,11 +11,31 @@ import { TodoService } from '../todo.service';
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
-  todoList: Todo[] = [];
+  todoList$!: Observable<Todo[]>;
+  text: string = '';
+  date = new Date();
+  conditions$!: Observable<string>;
 
-  constructor(private todoService: TodoService) {}
+  constructor(
+    private todoService: TodoService,
+    private weatherService: WeatherService
+  ) {}
 
   ngOnInit(): void {
-    this.todoList = this.todoService.getTodos();
+    this.todoList$ = this.todoService.getAllTodo();
+    this.conditions$ = this.weatherService.getCurrentConditions();
+  }
+
+  handleRemove(id: number) {
+    this.todoList$ = this.todoService.removeTodo(id);
+  }
+
+  handleUpdate(todo: Todo) {
+    this.todoList$ = this.todoService.updateTodo(todo);
+  }
+
+  submit() {
+    this.todoList$ = this.todoService.addTodo(this.text);
+    this.text = '';
   }
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { cloneDeep } from 'lodash-es';
+
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Todo } from '../shared/todo';
+import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
+  @Input() todo!: Todo;
+  @Output() remove = new EventEmitter();
+  @Output() update = new EventEmitter();
 
-  constructor() { }
+  todoChecked!: FormControl;
+  todoInput!: FormControl;
+
+  form!: FormGroup;
+
+  constructor(private todoService: TodoService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.todoChecked = new FormControl(this.todo.done);
+    this.todoInput = new FormControl(this.todo.text);
+    this.form = this.fb.group({
+      todoChecked: this.todoChecked,
+      todoInput: this.todoInput,
+    });
   }
 
+  submit() {
+    const todo: Todo = {
+      id: this.todo.id,
+      done: !!this.form.value.todoChecked,
+      text: this.form.value.todoInput,
+    };
+    this.todoService.updateTodo(todo).subscribe((res) => {
+      console.log(res);
+    });
+  }
+
+  handleRemove() {
+    const { id } = this.todo;
+    this.remove.emit(id);
+  }
+
+  changeDone() {
+    const todo: Todo = {
+      id: this.todo.id,
+      text: this.form.value.todoInput,
+      done: this.form.value.todoChecked,
+    };
+    this.update.emit(todo);
+  }
 }
