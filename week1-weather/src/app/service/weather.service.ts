01c5d133f2633828a9617c88997ce9e3df340778
rudy <rudy@jaranda.kr>
2021-11-06 03:24:49 +0900

    API url modify

--- week1-weather/src/app/service/weather.service.ts
+++ week1-weather/src/app/service/weather.service.ts
@@ -8,15 +8,12 @@ export class WeatherService {
     constructor(private http: HttpClient) {}
 
     /*
-        https://openweathermap.org/current
+        API 기존 정보 : https://openweathermap.org/current
     */ 
     getWeather(targetCity: String): Observable<any[]>  {
-        // const params = new HttpParams()
-        // .set('q','seoul').set('appid', environment.apiKey)
-        const url: string = environment.apiUrl+'?q=seoul&appid='+environment.apiKey;
-        console.log('url ' + url);
+        const params = new HttpParams().set('q','seoul').set('appid', environment.apiKey)
         try {
-			return this.http.get<any>(url).pipe();
+			return this.http.get<any>(environment.apiUrl, {params}).pipe();
 		} catch (e) {
 			console.log('eeee', e);
             throw new Error("Unexpected object: " + e);
